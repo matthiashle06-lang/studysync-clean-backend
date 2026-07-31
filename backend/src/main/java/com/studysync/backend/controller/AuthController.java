2c66b511d7fcb93c7e03e16fc003f41a885668b0
Matthias <matthias.hle06@gmail.com>
2026-07-31 09:56:23 +0800

    Capstone test

--- backend/src/main/java/com/studysync/backend/controller/AuthController.java
+++ backend/src/main/java/com/studysync/backend/controller/AuthController.java
@@ -5,7 +5,7 @@ import com.studysync.backend.repository.UserTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+//capstone
 import java.util.Optional;
 
 @RestController
